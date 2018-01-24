70e0e13b28f2580f585d308031a055cf647d04bd
Alain Lompo <alain.lompo@gmail.com>
2018-01-24 23:30:34 +0100

    INTRO: managing logging dependencies

--- vertx-wiki/src/main/java/io/vertx/starter/MainVerticle.java
+++ vertx-wiki/src/main/java/io/vertx/starter/MainVerticle.java
@@ -2,8 +2,23 @@ package io.vertx.starter;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
+import io.vertx.core.json.JsonObject;
+import io.vertx.core.logging.LoggerFactory;
+import io.vertx.ext.jdbc.JDBCClient;
+import org.slf4j.Logger;
 
 public class MainVerticle extends AbstractVerticle {
+
+  private static final String SQL_CREATE_PAGES_TABLE = "create table if not exists Pages (Id integer identity primary key, Name varchar(255) unique, Content clob)";
+  private static final String SQL_GET_PAGE = "select Id, Content from Pages where Name = ?";
+  private static final String SQL_CREATE_PAGE = "insert into Pages values (NULL, ?, ?)";
+  private static final String SQL_SAVE_PAGE = "update Pages set Content = ? where Id = ?";
+  private static final String SQL_ALL_PAGES = "select Name from Pages";
+  private static final String SQL_DELETE_PAGE = "delete from Pages where Id = ?";
+
+
+  private JDBCClient dbClient;
+  private static final Logger LOGGER = LoggerFactory.getLogger(MainVerticle.class);
 
   /*@Override
   public void start() {
@@ -26,7 +41,32 @@ public class MainVerticle extends AbstractVerticle {
 
   private Future<Void> prepareDatabase() {
     Future<Void> future = Future.future();
-    // (...)
+    // Completing the implementation below
+
+    dbClient = JDBCClient.createShared(vertx, new JsonObject()
+    .put("url", "jdbc:hsqldb:file:db/wiki")
+      .put("driver_class", "org.hsqldb.jdbcDriver")
+      .put("max_pool_size", "30")
+    );
+
+    dbClient.getConnection(ar -> {
+      if (ar.failed()) {
+        LOGGER.error("Could not open a db connection", ar.cause());
+        future.fail(ar.cause());
+      } else {
+        SQLConnection connection = ar.result();
+        connection.execute(SQL_CREATE_PAGES_TABLE, create -> {
+          connection.close();
+          if (create.failed()) {
+            LOGGER.error("Db prep error", create.cause());
+            future.fail(create.cause());
+          } else {
+            future.complete();
+          }
+        });
+      }
+    });
+
     return future;
   }
 
