a76619312342cbaf683c8fdc26af43d7f2ccdeea
Alain Lompo <alain.lompo@gmail.com>
2018-01-24 22:46:15 +0100

    Building the template module self
    Creating skeleton structure for DB and Http server preps

--- vertx-wiki/src/main/java/io/vertx/starter/MainVerticle.java
+++ vertx-wiki/src/main/java/io/vertx/starter/MainVerticle.java
@@ -1,14 +1,32 @@
 package io.vertx.starter;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
 
 public class MainVerticle extends AbstractVerticle {
 
-  @Override
+  /*@Override
   public void start() {
     vertx.createHttpServer()
         .requestHandler(req -> req.response().end("Hello Vert.x!"))
         .listen(8080);
+  }*/
+
+  @Override
+  public void start(Future<Void> startFuture) throws Exception{
+    startFuture.complete();
   }
 
+  private Future<Void> prepareDatabase() {
+    Future<Void> future = Future.future();
+    // (...)
+    return future;
+  }
+
+  private Future<Void> startHttpServer() {
+    Future<Void> future = Future.future();
+    // (...)
+    return future;
+  }
+  
 }
