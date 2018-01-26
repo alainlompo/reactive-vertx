ea1d05e1d97610d303bf7a865e6d93a5933d3128
Alain Lompo <alain.lompo@gmail.com>
2018-01-26 09:31:01 +0100

    INTRO-0011: Implementing the page update handler

--- vertx-wiki/src/main/java/io/vertx/starter/MainVerticle.java
+++ vertx-wiki/src/main/java/io/vertx/starter/MainVerticle.java
@@ -197,7 +197,35 @@ public class MainVerticle extends AbstractVerticle {
   }
 
   private void pageUpdateHandler(RoutingContext context) {
-    // TODO: do it
+    String id = context.request().getParam("id");
+    String title = context.request().getParam("title");
+    String markdown = context.request().getParam("markdown");
+    boolean newPage = "yes".equals(context.request().getParam("newPage"));
+
+    dbClient.getConnection(car -> {
+      if (car.succeeded()) {
+        SQLConnection connection = car.result();
+        String sql = newPage ? SQL_CREATE_PAGE : SQL_SAVE_PAGE;
+        JsonArray params = new JsonArray();
+        if (newPage) {
+          params.add(title).add(markdown);
+        } else {
+          params.add(markdown).add(id);
+        }
+        connection.updateWithParams(sql, params, res -> {
+          connection.close();
+          if (res.succeeded()) {
+            context.response().setStatusCode(303);
+            context.response().putHeader("Location", "/wiki/" + title);
+            context.response().end();
+          } else {
+            context.fail(res.cause());
+          }
+        });
+      } else {
+        context.fail(car.cause());
+      }
+    });
   }
 
   private void pageCreateHandler(RoutingContext context) {
