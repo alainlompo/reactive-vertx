0fb785e76656aa1bac1ce847c659e003b1d0bc4a
Alain Lompo <alain.lompo@gmail.com>
2018-02-10 14:31:19 +0100

    RFT-0004: Implementing separate verticle classes (W.I.P)

--- wikipeaks/src/main/java/org/techschulung/wikipeaks/WikiDatabaseVerticle.java
+++ wikipeaks/src/main/java/org/techschulung/wikipeaks/WikiDatabaseVerticle.java
@@ -1,6 +1,11 @@
 package org.techschulung.wikipeaks;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.core.eventbus.Message;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.jdbc.JDBCClient;
+import io.vertx.ext.sql.SQLConnection;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,14 +19,89 @@ import java.util.Properties;
  * Created by alompo on 30.01.18.
  */
 public class WikiDatabaseVerticle extends AbstractVerticle {
+
     public static final String CONFIG_WIKIDB_JDBC_URL = "wikidb.jdbc.url";
     public static final String CONFIG_WIKIDB_JDBC_DRIVER_CLASS = "wikidb.jdbc.driver_class";
     public static final String CONFIG_WIKIDB_JDBC_MAX_POOL_SIZE = "wikidb.jdbc.max_pool_size";
     public static final String CONFIG_WIKIDB_SQL_QUERIES_RESOURCE_FILE = "wikidb.sqlqueries.resource.file";
-
     public static final String CONFIG_WIKIDB_QUEUE = "wikidb.queue";
 
     private static final Logger LOGGER = LoggerFactory.getLogger(WikiDatabaseVerticle.class);
+
+    private final HashMap<SqlQuery, String> sqlQueries = new HashMap<>();
+
+    private JDBCClient dbClient;
+
+    public enum ErrorCodes {
+        NO_ACTION_SPECIFIED,
+        BAD_ACTION,
+        DB_ERROR
+    }
+
+    @Override
+    public void start(Future<Void> startFuture) throws Exception {
+
+        /*
+            Note: this uses blocking APIs, but datas is small...
+         */
+        loadSqlQueries();
+
+        dbClient = JDBCClient.createShared(vertx, new JsonObject()
+                .put("url", config().getString(CONFIG_WIKIDB_JDBC_URL, "jdbc:hsqldb:file:db/wiki"))
+                .put("driver_class", config().getString(CONFIG_WIKIDB_JDBC_DRIVER_CLASS,"org.hsqldb.jdbcDriver"))
+                .put("max_pool_size", config().getInteger(CONFIG_WIKIDB_JDBC_MAX_POOL_SIZE,30)));
+
+        dbClient.getConnection(ar -> {
+            if (ar.failed()) {
+                LOGGER.error("Could not open a database connection", ar.cause());
+                startFuture.fail(ar.cause());
+            } else {
+                SQLConnection connection = ar.result();
+                connection.execute(sqlQueries.get(SqlQuery.CREATE_PAGES_TABLE), create -> {
+                    connection.close();
+                    if (create.failed()) {
+                        LOGGER.error("Database preparation error", create.cause());
+                        startFuture.fail(create.cause());
+                    } else {
+                        vertx.eventBus().consumer(config().getString(CONFIG_WIKIDB_QUEUE, "wikidb.queue"), this::onMessage);
+                        startFuture.complete();
+                    }
+                });
+            }
+        });
+    }
+
+    public void onMessage(Message<JsonObject> message) {
+
+        if (!message.headers().contains("action")) {
+            LOGGER.error("No action header specified for message with headers {} and body {}",
+                    message.headers(), message.body().encodePrettily());
+            message.fail(ErrorCodes.NO_ACTION_SPECIFIED.ordinal(), "No action header specified");
+            return;
+        }
+
+        String action = message.headers().get("action");
+
+        switch(action) {
+            case "all-pages":
+                fetchAllPages(message);
+                break;
+            case "get-page":
+                fetchPage(message);
+                break;
+            case "create-page":
+                createPage(message);
+                break;
+            case "save-page":
+                savePage(message);
+                break;
+            case "delete-page":
+                deletePage(message);
+                break;
+            default:
+                message.fail(ErrorCodes.BAD_ACTION.ordinal(), "Bad action: " + action);
+        }
+    }
 
     private enum SqlQuery {
         CREATE_PAGES_TABLE,
@@ -33,7 +113,7 @@ public class WikiDatabaseVerticle extends AbstractVerticle {
         SEARCH_ALL_PAGES
     }
 
-    private final HashMap<SqlQuery, String> sqlQueries = new HashMap<>();
+
 
     // TODO: Look for a better way to do this
     private void loadSqlQueries() throws IOException {
@@ -58,5 +138,6 @@ public class WikiDatabaseVerticle extends AbstractVerticle {
         sqlQueries.put(SqlQuery.DELETE_PAGE, queriesProps.getProperty("delete-page"));
         sqlQueries.put(SqlQuery.SEARCH_ALL_PAGES, queriesProps.getProperty("search-all-pages"));
     }
+
 
 }
