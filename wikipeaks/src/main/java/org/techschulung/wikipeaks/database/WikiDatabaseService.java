cc05bdf0f4e644c8c55dee0de320e79ba967684f
Alain Lompo <alain.lompo@gmail.com>
2018-04-04 21:15:18 +0200

    RFS-0004: Database service interface and implementation

---
+++ wikipeaks/src/main/java/org/techschulung/wikipeaks/database/WikiDatabaseService.java
@@ -0,0 +1,38 @@
+package org.techschulung.wikipeaks.database;
+
+import io.vertx.codegen.annotations.Fluent;
+import io.vertx.codegen.annotations.ProxyGen;
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Handler;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.jdbc.JDBCClient;
+
+import java.util.HashMap;
+
+/**
+ * Created by alompo on 03.04.18.
+ */
+@ProxyGen
+public interface WikiDatabaseService {
+
+    @Fluent
+    WikiDatabaseService fetchAllPages(Handler<AsyncResult<JsonArray>> resultHandler);
+
+    @Fluent
+    WikiDatabaseService fetchPage(String name, Handler<AsyncResult<JsonObject>> resultHandler);
+
+    @Fluent
+    WikiDatabaseService createPage(String title, String markdown, Handler<AsyncResult<Void>> resultHandler);
+
+    @Fluent
+    WikiDatabaseService savePage(String id, String markdown, Handler<AsyncResult<Void>> resultHandler);
+
+    @Fluent
+    WikiDatabaseService deletePage(int id, Handler<AsyncResult<Void>> resultHandler);
+
+    static WikiDatabaseService create(JDBCClient dbClient, HashMap<SqlQuery, String> sqlQueries, Handler<AsyncResult<WikiDatabaseService>> readyHandler ) {
+        return new WikiDatabaseServiceImpl(dbClient, sqlQueries,readyHandler);
+    }
+}
